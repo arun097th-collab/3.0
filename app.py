d8a631573fb9321adb02827d89196f6cb726f524
arun097th-collab <arun097th@gmail.com>
2026-05-23 18:17:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import telebot
 from flask import Flask
 
+TOKEN = "8852863411:AAHJeN2b7oHdWedNjG1wTb0uNYSSgs3JK4A"
+
+bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Bot Running 🚀")
 
 @app.route("/")
 def home():
     return "Bot Running"
 
 if __name__ == "__main__":
-    app.run()
+    bot.infinity_polling()
