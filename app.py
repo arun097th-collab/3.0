e0a6874d107d47b0531dcbae3fda7ed07f3f4a2e
arun097th-collab <arun097th@gmail.com>
2026-05-24 01:16:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,16 @@ def start(message):
 @bot.message_handler(content_types=['video', 'document'])
 def get_video(message):
 
-    try:
-        if message.video:
-            file_id = message.video.file_id
-        else:
-            file_id = message.document.file_id
+    if message.video:
+        file_id = message.video.file_id
+    else:
+        file_id = message.document.file_id
 
-        file_info = bot.get_file(file_id)
+    file_info = bot.get_file(file_id)
 
-        link = f"https://api.telegram.org/file/bot{TOKEN}/{file_info.file_path}"
+    link = f"https://api.telegram.org/file/bot{TOKEN}/{file_info.file_path}"
 
-        bot.reply_to(message, f"Direct Link ✅\n\n{link}")
-
-    except Exception as e:
-        bot.reply_to(message, str(e))
+    bot.reply_to(message, f"Download Link ✅\n\n{link}")
 
 print("Bot Started...")
 bot.infinity_polling()
