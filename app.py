ba5693e85f8a366de8d0c8fa1c799f8b03ef7f33
arun097th-collab <arun097th@gmail.com>
2026-05-23 18:46:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Bot Running 🚀")
+    bot.reply_to(message, "Send Video URL")
+
+@bot.message_handler(func=lambda m: True)
+def get_link(message):
+
+    video_url = message.text
+
+    mx_link = f"""
+Open In MX Player👇
+
+intent://{video_url}#Intent;package=com.mxtech.videoplayer.ad;end
+"""
+
+    bot.reply_to(message, mx_link)
 
 print("Bot Started...")
 bot.infinity_polling()
